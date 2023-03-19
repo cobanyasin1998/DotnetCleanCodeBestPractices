2872c58267749b618ce97bfbb8d904d5f77e707e
Yasin Çoban <yasincoban1998@outlook.com>
2023-03-19 16:56:16 +0300

    Add project files.

---
+++ NamingConventions/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace NamingConventions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+        ProductManager productManager = new ProductManager();
+    }
+
+    class ProductManager//PascalCase
+    {
+        public ProductManager()//PascalCase
+        {
+
+        }
+    }
+
+
+    
+}
