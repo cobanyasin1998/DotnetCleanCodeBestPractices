623d1efb47ec0fc870fb801d85028daee1c2ed7d
Yasin Çoban <yasincoban1998@outlook.com>
2023-03-19 20:56:17 +0300

    teknik borçlanma - code smell - encapsulation

---
+++ CodeSmell/Program.cs
@@ -0,0 +1,171 @@
+﻿using System;
+
+namespace CodeSmell
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CustomerManager customerManager = new CustomerManager();
+
+            customerManager.Add(new Customer
+            {
+                FirstName = "Yasin",
+                LastName = "Çoban",
+                IdentityNumber = "1516",
+                CityId = 2
+            });
+            customerManager.Add(new Customer
+            {
+                FirstName = "Yasin",
+                LastName = "Çoban",
+                IdentityNumber = "1516"
+            });
+            customerManager.Add(new Customer
+            {
+                FirstName = "Yasin",
+                LastName = "Çoban",
+                IdentityNumber = "1516"
+            });
+            //Bad Using
+            //customerManager.Add("Yasin", "Çoban", "1516");
+
+
+            //First Name Min2 Karakter
+            customerManager.AddByOtherBusiness(new Customer
+            {
+                FirstName = "S",
+                LastName = "Çoban",
+                IdentityNumber = "1516"
+            });
+        }
+    }
+    class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+
+            //Bad Using 
+            //if (!String.IsNullOrEmpty(customer.FirstName) &&
+            //    !String.IsNullOrEmpty(customer.LastName) &&
+            //    !String.IsNullOrEmpty(customer.IdentityNumber))
+            //{
+            //    Console.WriteLine("Customer Added!");
+            //}
+            //Bad Using - Technical Debt
+
+            //if (String.IsNullOrEmpty(customer.FirstName) ||
+            //  String.IsNullOrEmpty(customer.LastName) ||
+            //  String.IsNullOrEmpty(customer.IdentityNumber))
+            //{
+            //    throw new Exception("Validasyon Hatası Oldu");
+            //}
+
+            ValidateFirstNameIfEmpty(customer);
+            ValidateLastNameIfEmpty(customer);
+
+            ValidateIdentityNumberIfEmpty(customer);
+
+            //    Console.WriteLine("Customer Added!");
+
+
+            //if (String.IsNullOrEmpty(customer.FirstName) ||
+            // String.IsNullOrEmpty(customer.LastName) ||
+            // String.IsNullOrEmpty(customer.IdentityNumber))
+            //{
+            //    throw new Exception("Validasyon Hatası Oldu");
+            //}
+            CustomerDal customerDal = new CustomerDal();
+            customerDal.Add(customer);
+        }
+        public void AddByOtherBusiness(Customer customer)
+        {
+            ValidateFirstNameIfEmpty(customer);
+            ValidateLastNameIfEmpty(customer);
+
+            ValidateIdentityNumberIfEmpty(customer);
+            ValidateFirstNameMinLengthTwo(customer);
+            CustomerDal customerDal = new CustomerDal();
+            customerDal.Add(customer);
+        }
+        //Bad Using
+        //public void Add(
+        //  string firstName,
+        //  string lastName,
+        //  string identityNumber,
+        //  int cityId)
+        //{
+        //    Console.WriteLine("Customer Added!");
+        //}
+
+        //Bad Using
+        //private void Validate(Customer customer)
+        //{
+        //    if (String.IsNullOrEmpty(customer.FirstName) ||
+        //   String.IsNullOrEmpty(customer.LastName) ||
+        //   String.IsNullOrEmpty(customer.IdentityNumber))
+        //    {
+        //        throw new Exception("Validasyon Hatası Oldu");
+        //    }
+        //}
+
+        private void ValidateFirstNameIfEmpty(Customer customer)
+        {
+            if (String.IsNullOrWhiteSpace(customer.FirstName))
+            {
+                throw new Exception("Validasyon Hatası Oldu");
+            }
+        }
+        private void ValidateLastNameIfEmpty(Customer customer)
+        {
+            if (String.IsNullOrWhiteSpace(customer.LastName))
+            {
+                throw new Exception("Validasyon Hatası Oldu");
+            }
+        }
+        private void ValidateIdentityNumberIfEmpty(Customer customer)
+        {
+            if (String.IsNullOrWhiteSpace(customer.IdentityNumber))
+            {
+                throw new Exception("Validasyon Hatası Oldu");
+            }
+        }
+        private void ValidateFirstNameMinLengthTwo(Customer customer)
+        {
+            if (customer.IdentityNumber.Length > 2)
+            {
+                throw new Exception("Validasyon Hatası Oldu");
+            }
+        }
+
+    }
+    class CustomerDal
+    {
+        public void Add(Customer customer)
+        {
+
+
+            Console.WriteLine("Customer Added!");
+
+
+
+
+        }
+    }
+    class Customer
+    {
+
+
+
+
+        public int Id { get; set; }
+        public int CityId { get; set; }
+
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string IdentityNumber { get; set; }
+
+    }
+}
--- NamingConventions/Program.cs
+++ NamingConventions/Program.cs
@@ -4,6 +4,10 @@ namespace NamingConventions
 {
     class Program
     {
+        /*
+         Detaylı Bilgi ->
+         https://github.com/ktaranov/naming-convention/blob/master/C%23%20Coding%20Standards%20and%20Naming%20Conventions.md
+         */
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
@@ -13,14 +17,24 @@ namespace NamingConventions
         ProductManager productManager = new ProductManager();
     }
 
+    /*Class'lar default olarak internal erişim belirteci alır*/
     class ProductManager//PascalCase
     {
+        /*class'in içindeki propertyler default olarak private erişim belirteci alır*/
+        int enBuyukSayi;//camelCase
+        /*
+         Propertyleri public yaparsak encapsulation yönünden hatalıdır
+         Public yaparsak property isimleri PascalCase olur
+         */
+        public int Sayi { get; set; }//PascalCase
         public ProductManager()//PascalCase
         {
 
         }
+
+        public void GetAllByCategoryId(int id /*camelCase*/)//PascalCase
+        {
+            int enKucukSayi;//camelCase
+        }
     }
-
-
-    
 }
